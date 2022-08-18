790fd6b6e3d202b4bebf5c3b1fc1879b167b5ee7
MurgrayDJ <murgraydj@gmail.com>
2022-08-18 17:34:37 -0400

    Completes JavaScript Exercise 04
    Required the removal of 0 or more values from a provided array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...argsToDelete) {
 
+    for(let i = 0; i < argsToDelete.length; i++){
+        for(let j = 0; j < theArray.length; j++){
+            if(theArray[j] === argsToDelete[i]){
+                theArray.splice(j, 1);
+            }
+        }
+    }
+
+    return theArray;
 };
 
 // Do not edit below this line
