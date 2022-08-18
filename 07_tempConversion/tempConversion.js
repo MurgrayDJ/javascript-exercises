e89b52b136dd9be9c244d0e4713e480ed7b86583
MurgrayDJ <murgraydj@gmail.com>
2022-08-18 18:24:31 -0400

    Completes JavaScript Exercise 07
    Requires 2 functions that convert from fahrenheit to celcius and from
    celcius to fahrenheit.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) * (5/9);
+  let cRounded = Math.round(cTemp * 10)/10;
+  return cRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = cTemp * (9/5) + 32;
+  let fRounded = Math.round(fTemp * 10)/10;
+  return fRounded;
 };
 
 // Do not edit below this line
