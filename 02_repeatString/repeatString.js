d49e9fb7e478e7208ccc2d23fcbade853e229aa4
MurgrayDJ <murgraydj@gmail.com>
2022-08-18 16:22:18 -0400

    Completes JavaScript Exercise 02
     Required completing a function that would repeat a string a specified
     number of times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(theString, aNum) {
+    let finalString = "";
+    
+    if(aNum < 0){
+        finalString = 'ERROR';
+    }
 
+    for(let i = 0; i < aNum; i++){
+        finalString = finalString + theString;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
