4117cad198b6e152cd2342941a1a49bbec7f7f09
MurgrayDJ <murgraydj@gmail.com>
2022-08-18 16:51:38 -0400

    Completes JavaScript Exercise 03
    Required a function to reverse a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(aString) {
+    let reversedString = '';
+    let stringLength = aString.length;
 
+    for(let i = aString.length-1; i >= 0; i--){
+        reversedString = reversedString + aString[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
