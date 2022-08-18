4f0adeba94953d29aaa1886056b9ca75054b62a7
MurgrayDJ <murgraydj@gmail.com>
2022-08-18 17:53:31 -0400

    Completes JavaScript Exercise 05
    Requires a function to return the sum of all numbers between two
    nonnegative integer values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(intA, intB) {
+    if(typeof intA != 'number' || 
+        typeof intB != 'number' ||
+        intA < 0 || intB < 0){
+        return 'ERROR';
+    }
+    
+    let startInt = 0;
+    let endInt = 0;
+    
+    if(intA <= intB){
+        startInt = intA;
+        endInt = intB;
+    }
+    else{
+        startInt = intB;
+        endInt = intA;
+    }
+    
+    let finalSum = 0;
+    
+    for(let i = startInt; i <= endInt; i++){
+        finalSum += i;
+    }
 
+    return finalSum;
 };
 
 // Do not edit below this line
