34b4cc209f2e90af039d6176a45ac61dce9b48e9
murgraydj <murgraydj@gmail.com>
2022-10-05 14:56:33 -0400

    Completes Exercise 12 - findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((prevPerson, curPerson) => {
+        let prevAge, curAge;
+        let currentYear = new Date().getFullYear();
 
+        if('yearOfDeath' in prevPerson){
+            prevAge = prevPerson.yearOfDeath - prevPerson.yearOfBirth;
+        }
+        else{
+            prevAge = currentYear - prevPerson.yearOfBirth;
+        }
+
+        if('yearOfDeath' in curPerson){
+            curAge = curPerson.yearOfDeath - curPerson.yearOfBirth;
+        }
+        else{
+            curAge = currentYear - curPerson.yearOfBirth;
+        }
+
+        return (prevAge > curAge) ? prevPerson : curPerson;
+    }, {})
+
+    return oldest;
 };
 
 // Do not edit below this line
