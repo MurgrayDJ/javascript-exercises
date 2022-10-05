ef0285bafffa319cb822b6b6d5a73b4de4f0e432
murgraydj <murgraydj@gmail.com>
2022-10-05 12:32:43 -0400

    Completes Exercise 9 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (aString) {
+    aString = aString.toLowerCase();
+    let strArray = [];
 
+    //Create array with only letters from string
+    //No punctuation or spaces
+    for(let i = 0; i < aString.length; i++){
+        let currentChar = aString[i];
+        if(currentChar.toUpperCase() != currentChar.toLowerCase()){
+            strArray.push(currentChar);
+        }
+    }
+    
+    //Check if string is a palindrome
+    let end = strArray.length - 1;
+    for(let start = 0; start <= end; start++){
+        if(strArray[start] !== strArray[end]){
+            return false;
+        }
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
