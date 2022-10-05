db46d2e2626f27fc41aa9915fa8152b9749cd7fa
murgraydj <murgraydj@gmail.com>
2022-10-05 11:44:26 -0400

    Completes Exercise 08 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(anArray) {
+  return anArray.reduce(add,0);
 };
 
-const multiply = function() {
-
+const multiply = function(anArray) {
+  return anArray.reduce((num1, num2) => num1 * num2, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(aNum) {
+  let total = 1;
+	while(aNum > 0){
+    total *= aNum;
+    aNum--;
+  }
+  return total;
 };
 
 // Do not edit below this line
