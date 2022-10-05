22d6cccbfc5f1ec291e404d82141d8c8b0a90345
murgraydj <murgraydj@gmail.com>
2022-10-05 13:43:16 -0400

    Completes Exercise 10 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(aNum) {
+    let theNum = parseInt(aNum);
+    if(theNum < 0 || isNaN(theNum)){
+        return "OOPS";
+    }
 
+    let num1 = 1;
+    let num2 = 0;
+    let currentSum = 1;
+    for(let i = 1; i < theNum; i++){
+        currentSum = num1 + num2;
+        num2 = num1;
+        num1 = currentSum;
+    }
+    return currentSum;
 };
 
 // Do not edit below this line
